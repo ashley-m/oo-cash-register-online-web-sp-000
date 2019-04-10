e3ee99ff28a0a62130b5250b4225e37d4cbea424
Ashley Miller <asm2fe@virginia.edu>
2019-04-10 21:03:43 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,11 +1,16 @@
 class CashRegister
+<<<<<<< HEAD
   attr_accessor :total, :discount, :last, :last_cost
+=======
+  attr_accessor :total, :discount
+>>>>>>> c99b39bda2815c299e2e66ae897d779c3bf67b4b
   attr_reader :list
 
   def initialize(disc = 0)
     @total = 0
     @discount = disc
     @list = []
+<<<<<<< HEAD
     @last = []
   end
 
@@ -16,11 +21,21 @@ class CashRegister
     end
     cost *= qt
     @last_cost = cost
+=======
+  end
+
+  def add_item(item, cost, qt = 0)
+    @list << item
+    if qt != 0
+      cost *= qt
+    end
+>>>>>>> c99b39bda2815c299e2e66ae897d779c3bf67b4b
     @total += cost
   end
 
   def apply_discount
     @total *= 0.01*(100-@discount)
+<<<<<<< HEAD
     if discount != 0
       return "After the discount, the total comes to $#{@total.truncate}."
     else
@@ -39,5 +54,7 @@ class CashRegister
     else
       @last.each {|x| @list.delete(x)}
     end
+=======
+>>>>>>> c99b39bda2815c299e2e66ae897d779c3bf67b4b
   end
 end
